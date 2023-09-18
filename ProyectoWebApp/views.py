b3ff248f1457b49e80963018c74bcd7f28064f85
Jose Luis <jlge30@gmail.com>
2023-09-19 00:07:08 +0200

    corregidos fallos html iniciales

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -6,15 +6,15 @@ def home(request):
     return render(request,'ProyectoWebApp/home.html' )
 
 def servicios(request): 
-    return render(request,'ProyectoWebApp/home.html' )
+    return render(request,'ProyectoWebApp/servicios.html' )
 
 def tienda(request): 
-    return HttpResponse("Tienda")
+    return render(request,'ProyectoWebApp/tienda.html' )
 
 def blog(request): 
     return render(request,'ProyectoWebApp/blog.html' )
 
 def contacto(request): 
-    return HttpResponse("Contacto")
+    return render(request,'ProyectoWebApp/contacto.html' )
 
 
