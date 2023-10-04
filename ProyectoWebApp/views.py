4a918197efba9a7d405f3b9acf2668d9abc6fcf2
Jose Luis <jlge30@gmail.com>
2023-10-04 23:08:44 +0200

    corrigiendo errores de carro

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 from servicios.models import Servicio
-
+from carro.carro import Carro
 
 # Create your views here.
 def home(request): 
+
+    carro = Carro(request)
     return render(request,'ProyectoWebApp/home.html' )
 
 
